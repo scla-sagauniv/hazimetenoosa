18afc9c9e2aca21c49597ef1da89072686712c54
nakashimashin <nakashimashin0517@gmail.com>
2023-12-15 23:28:57 +0900

    TreeItemコンポーネント作成(shin)

---
+++ src/components/TreeItem.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ChevronDownIcon from '@heroicons/react/24/outline/ChevronDownIcon';
+import FolderIcon from '@heroicons/react/24/outline/FolderIcon';
+
+interface TreeItemProps {
+  label: string;
+  children?: React.ReactNode;
+}
+
+
+export const TreeItem = ({ label, children }: TreeItemProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggle = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    return (
+      <div>
+        <div className="flex items-center" onClick={toggle}>
+          <ChevronDownIcon className={`w-5 h-5 transition-transform ${isOpen ? 'rotate-0' : '-rotate-90'}`} />
+          <FolderIcon className="w-5 h-5" />
+          <span className="ml-1">{label}</span>
+        </div>
+        {isOpen && children}
+      </div>
+    );
+  };
+  
--- src/pages/Memo.tsx
+++ src/pages/Memo.tsx
@@ -1,30 +1,4 @@
-import React, { useState } from "react";
-import ChevronDownIcon from '@heroicons/react/24/outline/ChevronDownIcon';
-import FolderIcon from '@heroicons/react/24/outline/FolderIcon';
-
-interface TreeItemProps {
-  label: string;
-  children?: React.ReactNode;
-}
-
-const TreeItem = ({ label, children }: TreeItemProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div>
-      <div className="flex items-center" onClick={toggle}>
-        <ChevronDownIcon className={`w-5 h-5 transition-transform ${isOpen ? 'rotate-0' : '-rotate-90'}`} />
-        <FolderIcon className="w-5 h-5" />
-        <span className="ml-1">{label}</span>
-      </div>
-      {isOpen && children}
-    </div>
-  );
-};
+import { TreeItem } from "@/components/TreeItem";
 
 export const Memo = () => {
   return (
