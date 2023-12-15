d5ed9b6abc92783cc702303c6673f312a7415871
cho <hrea12250815@gmail.com>
2023-12-15 21:25:18 +0900

    サインインタブのバリデーションとページ遷移を追加

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -16,7 +20,42 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs"
 
+type Inputs = {
+  email: string;
+  password: string;
+}
+
 export const Auth = () => {
+  const navigate = useNavigate();
+  const [errorMsg, setErrorMsg] = useState("")
+  
+  const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors }
+  } = useForm<Inputs>({
+      mode: 'onChange',
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) =>{
+    if (data.email === "email@gmail.com" && data.password === "password"){  //仮ID・パスワード
+        loginSuccess();
+    }else{
+        loginErrorMsg();
+    }
+    reset();
+  };
+
+  const loginSuccess = () => {
+    setErrorMsg("");
+    navigate("/memo");
+  }
+
+  const loginErrorMsg = () => {
+    setErrorMsg("ユーザーIDかパスワードが間違っています。");
+  }
+
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <Tabs defaultValue="signin" className="w-[400px]">
@@ -26,28 +65,43 @@ export const Auth = () => {
       </TabsList>
       <TabsContent value="signin">
         <Card>
-          <CardHeader>
-            <CardTitle className="flex justify-center">Sign in</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="email">E-mail</Label>
-              <CardDescription>
-                Please enter your e-mail address
-              </CardDescription>
-              <Input type="email" id="email" placeholder="Email address" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <CardDescription>
-                Please enter your password
-              </CardDescription>
-              <Input id="password" placeholder="Your password" />
-            </div>
-          </CardContent>
-          <CardFooter className="flex justify-center">
-            <Button>Sign in</Button>
-          </CardFooter>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <CardHeader>
+              <CardTitle className="flex justify-center">Sign in</CardTitle>
+            </CardHeader>
+            <CardDescription className="errorMsg">
+              {errorMsg}
+            </CardDescription>
+            <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="email">E-mail</Label>
+                  <CardDescription>
+                    Please enter your e-mail address
+                  </CardDescription>
+                  <Input type="email" id="email" placeholder="Email address" {...register('email')} />
+                  <ErrorMessage
+                    errors={errors}
+                    name="email"
+                    render={({ message }) => <p className="text-red-500">{message}</p>}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="password">Password</Label>
+                  <CardDescription>
+                    Please enter your password
+                  </CardDescription>
+                  <Input id="password" placeholder="Your password" {...register('password')} />
+                  <ErrorMessage
+                      errors={errors}
+                      name="password"
+                      render={({ message }) => <p className="text-red-500">{message}</p>}
+                  />
+                </div>
+            </CardContent>
+            <CardFooter className="flex justify-center">
+              <Button type="submit">Sign in</Button>
+            </CardFooter>
+          </form>
         </Card>
       </TabsContent>
       <TabsContent value="signup">
