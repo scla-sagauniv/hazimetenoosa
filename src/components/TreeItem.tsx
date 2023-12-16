27731962515552c3680e4485dbab97e52cc5e4ba
nakashimashin <nakashimashin0517@gmail.com>
2023-12-16 11:29:14 +0900

    PlusFolder(shin)

--- src/components/TreeItem.tsx
+++ src/components/TreeItem.tsx
@@ -4,8 +4,17 @@ import FolderIcon from '@heroicons/react/24/outline/FolderIcon';
 import DocumentIcon from '@heroicons/react/24/outline/DocumentIcon';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Node } from '@/types/index';
+import { FaFolderPlus } from 'react-icons/fa';
 
-export const TreeItem = ({ id, label, isSecret, isFolder, children, level = 0 }: Node) => {
+export const TreeItem = ({
+  id,
+  label,
+  isSecret,
+  isFolder,
+  children,
+  level = 0,
+  addNewFolder // ここで関数を受け取る
+}: Node & { addNewFolder?: (parentId: string) => void }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
@@ -14,8 +23,13 @@ export const TreeItem = ({ id, label, isSecret, isFolder, children, level = 0 }:
     }
   };
 
-  const indent = level * 20;
+  const handleAddFolder = () => {
+    if (addNewFolder) {
+      addNewFolder(id);
+    }
+  };
 
+  const indent = level * 20;
   const Icon = isFolder ? FolderIcon : DocumentIcon;
 
   if (isSecret) {
@@ -24,17 +38,22 @@ export const TreeItem = ({ id, label, isSecret, isFolder, children, level = 0 }:
 
   return (
     <div style={{ paddingLeft: `${indent}px` }}>
-      <div className="flex items-center" onClick={toggle}>
+      <div className="flex items-center">
         <Checkbox className="mr-2" />
         {isFolder && (
-          <ChevronDownIcon className={`w-5 h-5 transition-transform ${isOpen ? 'rotate-0' : '-rotate-90'}`} />
+          <ChevronDownIcon className={`w-5 h-5 mr-2 transition-transform ${isOpen ? 'rotate-0' : '-rotate-90'}`} onClick={toggle} />
         )}
-        <Icon className="w-5 h-5" />
+        {!isFolder && <Icon className="w-5 h-5 mr-2" />}
         <span className="ml-1">{label}</span>
+        {isFolder && <FaFolderPlus className='w-4 h-4 ml-2 cursor-pointer' onClick={handleAddFolder}/>}
       </div>
-      {isOpen && children && children.map((child) => (
-        <TreeItem key={child.id} {...child} level={level + 1} />
-      ))}
+      {isOpen && children && (
+        <div style={{ marginLeft: '20px' }}>
+          {children.map((child) => (
+            <TreeItem key={child.id} {...child} level={level + 1} addNewFolder={addNewFolder} /> // 子コンポーネントにも引き継ぐ
+          ))}
+        </div>
+      )}
     </div>
   );
 };
