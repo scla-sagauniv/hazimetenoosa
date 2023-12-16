83864038d9b3d5a6e4042e01e5fcab7de60e6f8c
cho <hrea12250815@gmail.com>
2023-12-16 14:01:19 +0900

    Contentコンポーネントを追加

--- src/pages/Memo.tsx
+++ src/pages/Memo.tsx
@@ -1,37 +1,6 @@
-// import { TreeItem } from '@/components/TreeItem'
-// import { Node } from '@/types/index'
-
-// const items: Node[] = [
-//   { id: "1", parentId: null, label: "Unread", isFolder: false, isSecret: false },
-//   { id: "2", parentId: null, label: "Threads", isFolder: true, isSecret: false, children: [
-//     { id: "2-1", parentId: "2", label: "General", isFolder: true, isSecret: false, children: [
-//       { id: "2-1-1", parentId: "2-1", label: "shin", isFolder: false, isSecret: true },
-//       { id: "2-1-2", parentId: "2-1", label: "shin2", isFolder: false, isSecret: false },
-//     ] },
-//     { id: "2-2", parentId: "2", label: "Random", isFolder: false, isSecret: false },
-//     { id: "2-3", parentId: "2", label: "Open Source Projects", isFolder: false, isSecret: true },
-//   ]},
-//   { id: "3", parentId: null, label: "Direct Messages", isFolder: true, isSecret: false, children: [
-//     { id: "3-1", parentId: "3", label: "Alice", isFolder: false, isSecret: false },
-//     { id: "3-2", parentId: "3", label: "Alice2", isFolder: false, isSecret: true },
-//     { id: "3-3", parentId: "3", label: "Bob2", isFolder: false, isSecret: false },
-//     { id: "3-4", parentId: "3", label: "Charlie2", isFolder: false, isSecret: false },
-//     { id: "3-5", parentId: "3", label: "Bob", isFolder: false, isSecret: true },
-//   ]},
-// ];
-
-// export const Memo = () => {
-//   return (
-//     <div>
-//       <div className='border-r border-blue-400 ml-2 p-2'>
-//         {items.map((item) => <TreeItem key={item.id} {...item} level={0} />)}
-//       </div>
-//     </div>
-//   );
-// };
-
 import { useState } from 'react'
 import { TreeItem } from '@/components/TreeItem'
+import { Content } from '@/components/Content';
 import { Node } from '@/types/index'
 
 export const Memo = () => {
@@ -102,12 +71,13 @@ export const Memo = () => {
   };
 
   return (
-    <div className='p-4'>
+    <div className='p-4 flex'>
       <div className='mt-4'>
         {items.map((item) => (
           <TreeItem key={item.id} {...item} level={0} addNewFolder={addNewFolder} />
         ))}
       </div>
+      <div className='p-4'><Content/></div>
     </div>
   );
 };
