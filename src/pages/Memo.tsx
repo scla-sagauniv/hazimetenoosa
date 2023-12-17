aed5f42bfa50c727abd6648268e4ae73b867437f
nakashimashin <nakashimashin0517@gmail.com>
2023-12-17 11:11:30 +0900

    型の再設計(shin)

--- src/pages/Memo.tsx
+++ src/pages/Memo.tsx
@@ -1,143 +1,210 @@
-import { useState } from 'react'
-import { TreeItem } from '@/components/TreeItem'
-import { Content } from '@/components/Content';
-import { Node } from '@/types/index'
-import { useStore } from '@/states/state';
+import { useState } from "react";
+import { TreeItem } from "@/components/TreeItem";
+import { Content } from "@/components/Content";
+import { Node, NodeComponent, TreeItemData } from "@/types/index";
+import { useStore } from "@/states/state";
 
 export const Memo = () => {
-  const selected = useStore((state) => state.selected)
-  const [items, setItems] = useState<Node[]>([
+  const selected = useStore((state) => state.selected);
+  const [items, setItems] = useState<TreeItemData[]>([]);
+  // {
+  //   node: {
+  //     id: 1,
+  //     parentId: null,
+  //     isFolder: false,
+  //     isSecret: false,
+  //     content: [
+  //       { id: 1, name: "test1", body: "", createdAt: new Date(), updatedAt: new Date(), deletedAt: null }
+  //     ]
+  //   },
+  //   isAdding: false,
+  //   isOpen: false
+  // },
+  // {
+  //   node: {
+  //     id: 2,
+  //     parentId: null,
+  //     isSecret: false,
+  //     isFolder: true,
+  //     children: [
+  //        {
+  //           id: 2.1,
+  //           parentId: 2,
+  //           isSecret: false,
+  //           isFolder: false,
+  //           content: [
+  //             { id: 2.1, name: "test2.1", body: "", createdAt: new Date(), updatedAt: new Date(), deletedAt: null }
+  //           ]
+  //         }
+  //     ],
+  //     content: [{id: 2, name: "test2", createdAt: new Date(), updatedAt: new Date(), deletedAt: null}]
+  //   },
+  //   isAdding: false,
+  //   isOpen: false
+  // }
+  const datas: Node[] = [
     {
-      id: "1",
+      id: 1,
       parentId: null,
-      label: "Unread",
       isFolder: false,
       isSecret: false,
-      isAdding: false,
-      isOpen: false,
+      content: {
+        id: 1,
+        name: "test1",
+        body: "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+      },
     },
     {
-      id: "2",
+      id: 2,
       parentId: null,
-      label: "Threads",
-      isFolder: true,
       isSecret: false,
-      isAdding: false,
-      isOpen: false,
+      isFolder: true,
       children: [
         {
-          id: "2-1",
-          parentId: "2",
-          label: "General",
-          isFolder: true,
+          id: 2.1,
+          parentId: 2,
           isSecret: false,
-          isAdding: false,
-          isOpen: false,
-          children: [
-            {
-              id: "2-1-1",
-              parentId: "2-1",
-              label: "shin",
-              isFolder: false,
-              isSecret: true,
-              isAdding: false,
-              isOpen: false,
-            },
-            {
-              id: "2-1-2",
-              parentId: "2-1",
-              label: "shin2",
-              isFolder: false,
-              isSecret: false,
-              isAdding: false,
-              isOpen: false,
-            },
-          ],
+          isFolder: false,
+          content: {
+            id: 2.1,
+            name: "test2.1",
+            body: "",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: null,
+          },
         },
       ],
+      content: {
+        id: 2,
+        name: "test2",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+      },
     },
-  ]);
+  ];
 
+  // transform Node[] to TreeItemData[]
+  setItems(
+    datas.map((data) => {
+      return {
+        id: data.id,
+        parentId: data.parentId,
+        isSecret: data.isSecret,
+        isFolder: data.isFolder,
+        children: data.children,
+        content: data.content,
+        isAdding: false,
+        isOpen: false,
+        level: 0,
+      };
+    })
+  );
 
-  const mapping = (item: Node, targetId: string): Node => {
+  const mapping = (item: TreeItemData, targetId: number): TreeItemData => {
     if (item.id === targetId) {
       return {
         ...item,
         isOpen: true,
       };
     } else if (item.children) {
-      const newChildren = item.children.map((child) => mapping(child, targetId));
-        return {
-        ...item,
-        children: newChildren,
-      };
-    }
-      return item;
-  }
-
-  const changeToOpen = (id: string) => {
-    setItems( items => {
-      return items.map((item) => {
-        return mapping(item, id);
-      })
-    } 
-    );
-  }
-
-
-  const changeToAdding = (id: string) => {
-    setItems(items => items.map(item => mappingForAdding(item, id)));
-  };
-  
-  const mappingForAdding = (item: Node, targetId: string): Node => {
-    if (item.id === targetId) {
+      const newChildren = item.children.map((child) =>
+        mapping(, targetId)
+      );
       return {
         ...item,
-        isAdding: !item.isAdding
-      };
-    } else if (item.children) {
-      const newChildren = item.children.map(child => mappingForAdding(child, targetId));
-      return {
-        ...item,
-        children: newChildren
+        children: {
+          children: newChildren,
+        },
       };
     }
     return item;
   };
 
-  const findParentNode = (parentId: string, nodes: Node[]): Node | null => {
+  const changeToOpen = (id: number) => {
+    setItems((items) => {
+      return items.map((item) => {
+        return mapping(item, id);
+      });
+    });
+  };
+
+  const changeToAdding = (id: number) => {
+    setItems((items) => items.map((item) => mappingForAdding(item, id)));
+  };
+
+  const mappingForAdding = (
+    item: NodeComponent,
+    targetId: number
+  ): NodeComponent => {
+    if (item.node.id === targetId) {
+      return {
+        ...item,
+        isAdding: !item.isAdding,
+      };
+    } else if (item.node.children) {
+      const newChildren = item.node.children.map((child) =>
+        mappingForAdding(child, targetId)
+      );
+      return {
+        ...item,
+        node: {
+          ...item.node,
+          children: newChildren,
+        },
+      };
+    }
+    return item;
+  };
+
+  const findParentNode = (
+    parentId: number,
+    nodes: NodeComponent[]
+  ): NodeComponent | null => {
     for (const node of nodes) {
-      if (node.id === parentId) {
+      if (node.node.id === parentId) {
         return node;
       }
-      if (node.children) {
-        const found = findParentNode(parentId, node.children);
+      if (node.node.children) {
+        const found = findParentNode(parentId, node.node.children);
         if (found) return found;
       }
     }
     return null;
   };
 
-  const addNodeToTree = (newNode: Node, nodes: Node[]): Node[] => {
+  const addNodeToTree = (
+    newNode: NodeComponent,
+    nodes: NodeComponent[]
+  ): NodeComponent[] => {
     return nodes.map((node) => {
-      if (node.id === newNode.parentId) {
+      if (node.node.id === newNode.node.parentId) {
         return {
           ...node,
-          children: node.children ? [...node.children, newNode] : [newNode],
+          children: node.node.children
+            ? [...node.node.children, newNode]
+            : [newNode],
         };
-      } else if (node.children) {
-        return { ...node, children: addNodeToTree(newNode, node.children) };
+      } else if (node.node.children) {
+        return {
+          ...node,
+          children: addNodeToTree(newNode, node.node.children),
+        };
       } else {
         return node;
       }
     });
   };
 
-  const addNewFolder = (parentId: string, folderName: string) => {
+  const addNewFolder = (parentId: number, folderName: number) => {
     const parentNode = findParentNode(parentId, items);
     const newLevel = parentNode?.level != null ? parentNode.level + 1 : 0;
-    const newFolder: Node = {
-      id: Date.now().toString(),
+    const newFolder: NodeComponent = {
+      node: Date.now(),
       parentId: parentId,
       label: folderName,
       isFolder: true,
@@ -152,7 +219,7 @@ export const Memo = () => {
     setItems(updatedItems);
   };
 
-  const addNewFile = (parentId: string, fileName: string) => {
+  const addNewFile = (parentId: number, fileName: string) => {
     const parentNode = findParentNode(parentId, items);
     const newLevel = parentNode?.level != null ? parentNode.level + 1 : 0;
     const newFile: Node = {
@@ -172,8 +239,12 @@ export const Memo = () => {
   };
 
   return (
-    <div className={`flex ${selected ? 'bg-black' : ''}`}>
-      <div className={`flex-1 ml-4 mt-4 overflow-auto ${selected ? 'text-white' : ''}`}>
+    <div className={`flex ${selected ? "bg-black" : ""}`}>
+      <div
+        className={`flex-1 ml-4 mt-4 overflow-auto ${
+          selected ? "text-white" : ""
+        }`}
+      >
         {items.map((item) => (
           <TreeItem
             key={item.id}
@@ -186,7 +257,9 @@ export const Memo = () => {
           />
         ))}
       </div>
-      <div className='mr-4 w-[80%]'><Content/></div>
+      <div className="mr-4 w-[80%]">
+        <Content />
+      </div>
     </div>
   );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,67 @@
+export type NodeComponent = {
+  node: Node;
+  isAdding: boolean;
+  isOpen: boolean;
+  level: number;
+};
+
+export type TreeItemData = {
+  id: number;
+  parentId: number | null;
+  isSecret: boolean;
+  isFolder: boolean;
+  children?: Node[];
+  content: Memo | Folder;
+  isAdding: boolean;
+  isOpen: boolean;
+  level: number;
+};
+
 export interface Node {
-    id: string
-    parentId: string | null
-    label: string
-    isSecret: boolean
-    isFolder: boolean
-    children?: Node[]
-    level?: number
-    isAdding: boolean
-    isOpen: boolean
-  }
+  id: number;
+  parentId: number | null;
+  isSecret: boolean;
+  isFolder: boolean;
+  children?: Node[];
+  content: Memo | Folder;
+}
+
+export type Memo = {
+  id: number;
+  name: string;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+};
+
+// export interface Node {
+//   id: string
+//   parentId: string | null
+//   label: string
+//   isSecret: boolean
+//   isFolder: boolean
+//   children?: Node[]
+//   level?: number
+//   isAdding: boolean
+//   isOpen: boolean
+// }
+
+export type Folder = {
+  id: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+};
 
 export type SigninInputs = {
-    email: string;
-    password: string;
-  }
-  
+  email: string;
+  password: string;
+};
+
 export type SignupInputs = {
-    email: string;
-    password: string;
-    repassword: string;
-  }
+  email: string;
+  password: string;
+  repassword: string;
+};
